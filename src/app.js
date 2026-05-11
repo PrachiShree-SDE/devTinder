aa9b1c8a7f80793dc88e5bcfda9c863c71c2a318
PrachiShree-SDE <prachishree111@gmail.com>
2026-05-11 20:22:52 +0530

    Play with route handler

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const express = require("express");
 const app = express();
 
-// this will only handle get call to /user
-app.get("/user/:userId/:name/:password", (req, res) => {
-    console.log(req.params);
-    res.send({
-        firstName: "Prachi",
-        lastName: "shree"
-    });
-}); 
+//app.use("/route", rH, [rH2, rH3],rH4,rH5); // rH-routeHandler
 
+app.use("/user", 
+    (req,res, next) => {
+    console.log("Handling route user!!");
+    //res.send("Response!!"); // after sending one res it will not move ahaid
+    next(); 
+},
+(req,res ) => {
+    console.log("Handling route user2!!");
+    res.send("2nd Response!!");
+   
+}
+)
 
 app.listen(7777, () => {
     console.log("Server is successfully listening at port number 7777...");
