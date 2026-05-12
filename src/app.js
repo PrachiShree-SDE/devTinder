2a65bb7430ce47a76c853af701a0aae27af65909
PrachiShree-SDE <prachishree111@gmail.com>
2026-05-12 22:40:32 +0530

    init-database

--- src/app.js
+++ src/app.js
@@ -1,34 +1,31 @@
 const express = require("express");
+const connectDB =  require("./config/database")
 const app = express();
-const {adminAuth,userAuth} = require("./middlewares/auth");
+const User = require("./models/user");
 
+app.post("/signup",async  (req, res) => {  
+    //Creating new instance of user model 
+    const user= new User({
+        firstName: "Viraat",
+        lastName:"Kohli",
+        emailId:"Viraat@email.com",
+        password:"Viraat@123"
+    });
+    try{
+         await user.save();
+    res.send("User Added successfully!");  
+    }catch(err){
+         res.status(400).send("Error saving the user:"+err.message); 
+    }   
+})      
 
-//Handle auth middleware for all GET, POST, ....request
-app.use("/admin",adminAuth);
-//app.use("/user",userAuth);
-//  app.use("/user",userAuth,(req,res)=>{
-//     res.send("User Data Sent");
-//  });
-
-app.use("/user/login",userAuth,(req,res)=>{
-    res.send("User Logged in successfully");
- });
-
- app.use("/user/data",userAuth,(req,res)=>{
-    res.send("User Data Sent");
- })
-
-// Get /user => midlleware chain => request handler 
-app.get("/admin/getAllData", (req,res) => {
-     //Logic of Check if the request is authorized
-        res.send("All Data Sent");
-});
-
-app.get("/admin/deleteUser", (req,res) => {
-     res.send("Deleted a user");
-});
-
-app.listen(7777, () => {
+connectDB().then(()=>{
+    console.log("Database connection established...");
+    app.listen(7777, () => {
     console.log("Server is successfully listening at port number 7777...");
-});
-  
+}); 
+}).catch(err => {
+    console.error("Database cannot be connected!!!");
+})   
+
+       
---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+     await mongoose.connect("mongodb+srv://prachishree111:Prachi11115@test1.4obii34.mongodb.net/devTinder  ");
+};
+
+module.exports=connectDB;
+
+
